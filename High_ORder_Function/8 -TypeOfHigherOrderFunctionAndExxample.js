673bd7affda028c6951ea358f12c0ff7e3083617
Babay <105317370+ImonDas1997@users.noreply.github.com>
2022-09-16 21:33:42 +0530

    new code

--- High_ORder_Function/8 -TypeOfHigherOrderFunctionAndExxample.js
+++ High_ORder_Function/8 -TypeOfHigherOrderFunctionAndExxample.js
@@ -60,3 +60,79 @@ let res2 = nums3.map(function (el)
   console.log(res2) //output is -->[ 9, 4, 16, 4, 1, 36 ]
 
 //-----------------------------------------------------------------------
+//****diff bw foreach and map */
+//since foreach dowsnot return array format value we we have to handle that situation in that way---->
+// forEach wont return you anything, 
+//it will return undefined 
+// map will return array,for exmple  no additional charges 
+//forEach there is additional charges 
+let foreachDoesnotReturnArr = [30, 20, 4, 2, 1, 6]
+let box= [] 
+foreachDoesnotReturnArr.forEach(function (el) 
+{  
+  box.push(el**2) //----> just pushing inside a empty arr and we are handling the situation
+  console.log(box)
+})
+
+
+// difference between forEach and map --> map will return array, where forEach will return undefined 
+
+
+//-----------------------------------------------------------------------------
+//if u write condition in the map you willl get boolean value as retun...empl below..
+let nums = [1, 2, 4, 21, 4, 23] 
+ let outPut = nums.map(function (el)
+  { 
+    return el%2==0
+  })  
+   console.log(outPut) // o/p->[ false, true, true, false, true, false ]
+   
+//--------------------------------------------------------------------------------
+//but we can prevent that situation using filter
+let n = [1, 2, 4, 21, 4, 23] 
+ let output = nums.filter(function (el)
+  { 
+    return el%2==0
+  })  
+   console.log(output)//o/p==>[ 2, 4, 4 ]
+
+// filter will return all elements which satis 
+// filter will return all true elements 
+// filter is also returning array 
+// array.filter (function (el,i,array))
+///**another exmple of  map and filter  ---------------->
+ 
+let n1 = ["abc","abcd","abcde","abcdef"]
+let o = n1.map(function (el)
+  { 
+    return el.length%2==0
+  })  
+   console.log(o) // o/p->[ false, true, false, true ]
+   //using filter
+   let n2 = ["abc","abcd","abcde","abcdef"]
+let oo = n2.filter(function (el)
+  { 
+    return el.length%2==0
+  })  
+   console.log(oo)/// o/p->[ 'abcd', 'abcdef' ]
+   
+
+//--------------------------------------------------------------------------------
+// reduce  new concept
+// nums.reduce (function (acc,el,i,array)) one new parameter name is accumulator always 1st parameter
+
+let num2 = [3, 26, 32, 321, 1]; 
+//without initial value it will take 1st array el as initial value
+ let result = num2.reduce (function (acc, el, i, arr) 
+ { 
+  return acc+el
+}) 
+ // first call -> acc=3 acc+el=3+26=29, acc = 29 storing it in acc
+ // 2nd call -> acc=29, acc+el 29+32=61 acc=61 
+ // 3rd call -> acc=61, acc+el=61+321=382 acc=382 
+ //4th call -> acc=382, acc+el=382+1=383|, acc=383 Ţthis final value
+
+
+
+
+//with any initual value

de13a6fe3db0c6a9fdc9272857d8a31cf032c014
Saheli Ghosh <sahelig59@gmail.com>
2022-09-24 19:43:57 +0530

    ..

---
+++ DSA_101/Pattern_Problems/5_Downward_Triangle_Star_Pattern.js
@@ -0,0 +1,18 @@
+// Downward Triangle Star Pattern
+// *****
+// ****
+// ***
+// **
+// *
+
+
+let n = 5;
+let string = "";
+for (let i = 0; i < n; i++) {
+  // printing star
+  for (let k = 0; k < n - i; k++) {
+    string += "*";
+  }
+  string += "\n";
+}
+console.log(string);

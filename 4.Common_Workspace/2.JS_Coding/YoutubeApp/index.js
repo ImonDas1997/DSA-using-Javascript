ee0c0c79092164f4e291e4329e3739a470294dac
Babay <105317370+ImonDas1997@users.noreply.github.com>
2022-10-20 02:46:15 +0530

    Co-authored-by: Saheli Ghosh <saheli1998ghosh@users.noreply.github.com>

--- 4.Common_Workspace/2.JS_Coding/YoutubeApp/index.js
+++ 4.Common_Workspace/2.JS_Coding/YoutubeApp/index.js
@@ -1,12 +1,19 @@
 //1. function defination for searchVideos ()
-const searchVideos = () => {
-     //.then, .catch 
-     //async await 
-     //is async await way of making api call? 
-     //is async await a way of handling a promise? 
+//2.fetch call
+//async arrow function in ES6
+const searchVideos = async () => {
+  //.then, .catch
+  //async await
+  //is async await way of making api call?
+  //is async await a way of handling a promise?
 
-let API_KEY = "AIzaSyB6_6cYQcRKe4kYQ1wvgiV-ULF-wRRXGGo";
+  const API_KEY = 'AIzaSyB6_6cYQcRKe4kYQ1wvgiV-ULF-wRRXGGo';
 
-let response = await fetch('https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=rrr&key=[AIzaSyB6_6cYQcRKe4kYQ1wvgiV-ULF-wRRXGGo]'); 
-
-    };
+  let search_term = document.getElementById("search_term").value;
+  let response = await fetch(
+    'https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${search_term}&key==${API_KEY}'
+  );
+  let data =await response.json(); //here .json means not converting just collecting data //bad syntax
+  console.log('data',data);
+};
+searchVideos();

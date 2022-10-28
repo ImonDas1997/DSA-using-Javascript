0a275f2a0c69b0064d91dadb961b8c7a6f61c24c
Saheli Ghosh <sahelig59@gmail.com>
2022-10-29 03:13:30 +0530

    .....

--- 4.Common_Workspace/2.JS_Coding/psc_3/gif_details.js
+++ 4.Common_Workspace/2.JS_Coding/psc_3/gif_details.js
@@ -1,3 +1,29 @@
 let id = JSON.parse(localStorage.getItem("details"));
-console.log(id);
- // const details = () => { // }
+ console.log(id);
+let apikey="XWJ36C1nSJZycIQbJLFaeidn3przWC6D";
+
+ const details = async () => {
+  try{
+let response =
+  await fetch(`https://api.giphy.com/v1/gifs/${id}?api_key=${apikey}`);
+  let data = await response.json();
+  //append(data);
+  console.log(data.data);
+  }
+  catch(error){
+console.log(error)
+  }
+
+      }
+details();
+
+const append=(data) => {
+    let details=documment.getElementId("details");
+    let img = document.createElement("img");
+    img.src = data.images.original.url;
+
+    let p = document.createElement("p");
+    p.innerHTML = data.title;
+    details.append(p, img);
+}
+
--- 4.Common_Workspace/2.JS_Coding/psc_3/giphy.js
+++ 4.Common_Workspace/2.JS_Coding/psc_3/giphy.js
@@ -35,3 +35,163 @@ const detail_gif = (id) => {
 localStorage.setItem("details", JSON.stringify(id));
 window.location.href = "/gif_details.html";
 };
+
+// random 
+//api- format for the url;
+
+const random = async  () => { 
+    let gif = document.getElementById("gif"); 
+    gif.innerHTML = null; 
+    try { 
+        let response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=$(apikey)&tag=&rating=g`);
+         let result = await response.json (); 
+         console.log(result); 
+         let img = document.createElement("img"); 
+         img.src = result.data.images.downsized.url;
+         img.addEventListener("click", ()=>{
+            detail_gif(data.data.id);
+         });
+
+         gif.append(img);
+        
+    }  catch (error) {
+   console.log(error);
+    }
+}
+
+//categories 
+//api- document.
+//api.giphy.com/v1/gifs/categories
+
+const categories = async  () => { 
+    let gif = document.getElementById("gif"); 
+    gif.innerHTML = null;
+
+    let soritng = document.getElementById("sorting");
+     sorting.innerHTML = null;
+
+
+    try{
+ let response = await fetch(
+   `https://api.giphy.com/v1/gifs/categories?api_key=$(apikey)&tag=&rating=g`
+ );
+         let result = await response.json(); 
+
+         let sorting_Z_A = document.createElement("button");
+         sorting_Z_A.innerText = "Z-A";
+         sorting.append(sorting_Z_A);
+
+         let sorting_A_Z = document.createElement("button");
+         sorting_A_Z.innerText = "A-Z";
+         sorting.append(sorting_A_Z);
+
+         let dum;
+         sorting_A_Z.onclick = () => {
+           sorting_cat((dum = false));
+         };
+         sorting_Z_A.onclick = () => {
+           sorting_cat((dum = true));
+         };
+
+         console.log(result);
+
+result.data.forEach((e) => {
+ let name = document.createElement("p"); 
+ name.innerHTML = e.name;
+ // console.log(name) 
+let image = document.createElement("img");
+ image.src = e.gif.images.downsized.url; 
+ image.addEventListener("click", () => { detail_gif(e.gif.id); 
+}); 
+gif.append(name, image);
+});
+
+    } 
+    catch(error){
+console.log(error);
+    }
+}
+const sorting_cat = (dum) => {
+  let data = JSON.parse(localStorage.getItem("categories"));
+
+  // it will only run when dum is true ;
+  if (dum == true) {
+    data = data.reverse();
+  }
+
+  // the below code will run every time we click on sorting_cat.
+  let gif = document.getElementById("gif");
+  gif.innerHTML = null;
+
+  data.forEach((e) => {
+    let name = document.createElement("p");
+    name.innerHTML = e.name;
+    // console.log(name)
+    let image = document.createElement("img");
+    image.src = e.gif.images.downsized.url;
+    image.addEventListener("click", () => {
+      detail_gif(e.gif.id);
+    });
+    gif.append(name, image);
+  });
+}
+
+const search = async () => {
+     try { 
+        let gif = document.getElementById("gif"); 
+        gif.innerHTML = null;
+        let query = document.getElementById("search").value; 
+        if (query == "") { 
+            alert("please provide your input"); 
+        }
+            let res = await fetch(
+              `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=25&offset=0&rating=g&lang=en`
+            );
+
+            let data = await res.json(); console.log(data.data); 
+            data.data.forEach((e) => {
+                 let name = document.createElement("p");
+                  name.innerHTML = e.title;
+                   // console.log(name)
+                   let image = document.createElement("img"); 
+                   image.src = e.images.downsized.url;
+              image.addEventListener("click", () =>{ 
+                detail_gif(e.id);
+                 }); 
+                 gif.append(name, image); 
+                });  
+            } catch (e) { 
+                console.log(e); 
+            }  
+        
+    };
+
+    const translates = async () => {
+      try {
+        let gif = document.getElementById("gif");
+        gif.innerHTML = null;
+        let query = document.getElementById("search").value;
+        if (query == "") {
+          alert("please provide your input");
+        }
+        let res = await fetch(
+          `https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&q=${query}&limit=25&offset=0&rating=g&lang=en`
+        );
+
+        let data = await res.json();
+        console.log(data);
+       
+          let name = document.createElement("p");
+          name.innerHTML = data.data.title;
+          // console.log(name)
+          let image = document.createElement("img");
+          image.src = data.data.images.downsized.url;
+          image.addEventListener("click", () => {
+            detail_gif(data.data.id);
+          });
+          gif.append(name, image);
+       
+      } catch (e) {
+        console.log(e);
+      }
+    };

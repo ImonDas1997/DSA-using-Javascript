92255c75571eea3d81e0bb5a64f104da7b6f1c53
Saheli Ghosh <sahelig59@gmail.com>
2022-10-30 16:10:29 +0530

    done

---
+++ 4.Common_Workspace/2.JS_Coding/Import_export/user.js
@@ -0,0 +1,35 @@
+// import { navbar } from "./2JS_Coding./components/navbar.js";
+// document.body.innerHTML = navbar();
+
+// async function getData() {
+//   let parent = document.getElementById("data");
+//   let res = await fetch("http://jsonplaceholder.typicode.com/posts");
+//   let data = await res.json();
+//   append(data, parent);
+// }
+
+// function append(data, parent) {
+//   data.forEach((el) => {
+//     let p = document.createElement("p");
+//     p.innerText = el.title;
+//     parent.appendChild(p);
+//   });
+// }
+// getData();
+
+
+
+import { getData, append } from "./scripts/showData.js";
+import header from "./components/header.js";
+
+navbar().then((res) => {
+  document.body.innerHTML = res;
+  let response = getData("http://jsonplaceholder.typicode.com/posts");
+
+  response.then((data) => {
+    let parent = document.getElementById("data");
+    console.log("parent : ", parent);
+
+    append(data, parent, "posts");
+  });
+});
---
+++ 4.Common_Workspace/2.JS_Coding/asynchronous_program/test.js
@@ -0,0 +1,12 @@
+function test() { 
+     document.body.append("Hello, I am here");
+
+}
+     console.log(1) 
+     console.log(2) 
+      console.log(3) 
+     console.log(4) 
+     
+   var id= setInterval(test, 3000); 
+
+   clearInterval(id);

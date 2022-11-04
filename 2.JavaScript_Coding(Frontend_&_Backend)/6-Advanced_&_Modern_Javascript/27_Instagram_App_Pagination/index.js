e901e0cf464294e7bf454f1a9f224fdddf283a4e
Babay <105317370+ImonDas1997@users.noreply.github.com>
2022-11-05 03:03:55 +0530

    Co-authored-by: Saheli Ghosh <saheli1998ghosh@users.noreply.github.com>

--- 2.JavaScript_Coding(Frontend_&_Backend)/6-Advanced_&_Modern_Javascript/27_Instagram_App_Pagination/index.js
+++ 2.JavaScript_Coding(Frontend_&_Backend)/6-Advanced_&_Modern_Javascript/27_Instagram_App_Pagination/index.js
@@ -7,18 +7,30 @@ navbar_div.innerHTML = navbar();
 //append will need two things
 //1. data
 let posts_div = document.getElementById("posts");
+
 const getData = async () => {
   let res = await fetch("http://localhost:3000/posts");
   let data = await res.json();
   append(data, posts_div);
-  createButtons(data.length,2);
+  createButtons(data.length, 2);
 };
 // http://localhost:3000/posts →> JON
 
+const getPaginatedData = async (clicked_button, limit) => {
+  let res = await fetch(
+    `http://localhost:3000/posts?_page=${clicked_button}&_limit=${limit}`
+  );
+  let data = await res.json();
+  append(data, posts_div);
+};
+
 getData();
+//to append initial data by default
+getPaginatedData(1, 2);
+
 //if we have 100 images
- //2 images per page
-  let buttons_div = document.getElementById('buttons');
+//2 images per page
+let buttons_div = document.getElementById("buttons");
 
 const createButtons = (total_images, images_per_page) => {
   const buttons = Math.ceil(total_images / images_per_page);
@@ -31,7 +43,7 @@ const createButtons = (total_images, images_per_page) => {
     btn.innerText = i;
     //kartawya
     btn.onclick = () => {
-      console.log(i);
+      getPaginatedData(i, 2);
     };
     buttons_div.append(btn);
   }

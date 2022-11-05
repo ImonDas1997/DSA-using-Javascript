dd4919cb50be79ecd8ec5fd3d0fab98006aad3b2
Babay <105317370+ImonDas1997@users.noreply.github.com>
2022-11-06 03:25:21 +0530

    Co-authored-by: Saheli Ghosh <saheli1998ghosh@users.noreply.github.com>

---
+++ 2.JavaScript_Coding(Frontend_&_Backend)/6-Advanced_&_Modern_Javascript/17_Movieapp_resusableComponent/components/navbar.js
@@ -0,0 +1,17 @@
+function navbar() {
+  return '<div id="lhs"> <a href="/index.html"> <img src="https://st1.bgr.in/wp-content/uploads/2020/03/Hotstar-new-logo.jpg?impolicy=Medium_Widthonly&w=340" alt=""/> <h4>TV</h4> <h4>Movies</h4> <h4>Sports</h4> <h4>PRYME</h4> </div> <div id="rhs"> <a href="search.html"><button>Search Movies </div>';
+}
+
+
+
+
+    <div id="lhs"> <a href="/index.html"> <img src="https://st1.bgr.in/wp-content/uploads/2020/03/Hotstar-new-logo.jpg?impolicy=Medium_Widthonly&w=340" alt=""/> 
+        <h4>TV</h4>
+         <h4>Movies</h4> 
+         <h4>Sports</h4>
+          <h4>PRYME</h4> 
+        </div>
+      
+          <a href="search.html">
+            <button>Search Movies </button></a>
+            </div>

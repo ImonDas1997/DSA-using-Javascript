88facf6654859e9c5795f131d56a978d19b7e401
Babay <105317370+ImonDas1997@users.noreply.github.com>
2022-11-05 23:53:02 +0530

    ...

--- 2.JavaScript_Coding(Frontend_&_Backend)/6-Advanced_&_Modern_Javascript/15_MovieApp_searchEngine/13_MovieApp_api_Integration(OMDB_api)SearchFUction/search.js
+++ 2.JavaScript_Coding(Frontend_&_Backend)/6-Advanced_&_Modern_Javascript/15_MovieApp_searchEngine/13_MovieApp_api_Integration(OMDB_api)SearchFUction/search.js
@@ -1,21 +1,48 @@
+//we are implementing debouncing techique here.
+
+//serch api with poarameter
+//a
+//av
+//ave
+//aven
+//avege
+//avengers
+//we will bounce 1 ,2 ,3 records and so on because previous api calls are invalid once we type new keyward we need new reselt from api....
+
 async function search_movie() {
   let loder_div = document.getElementById("loader_div");
   loder_div.style.display = "block";
-  let movie_name = document.getElementById("movie_name").value;
+  let movie_name = document.getElementById("live_search").value;
   try {
     let response = await fetch(
       `https://www.omdbapi.com/?apikey=c32921b4&s=${movie_name}`
     );
-    console.log(response);
+
     let data = await response.json();
     let actual_data = data.Search; //search is the key inside the data object..if u go inside and see from responce u will see that ..
-
-    console.log(actual_data);
     append_movie(actual_data);
   } catch (err) {
     console.log(err);
   }
 }
+
+//----------------------------------------------------------
+let id;
+function debounce(func, delay) {
+  //func-> searchMovies ()
+  //a->debounce->sTimeout('a')->7-> searchMovies ('a')
+  //av-> debounce->setTimeout('av')->searchMovies ('av')
+  if (id) {
+    clearTimeout(id);
+  }
+  id = setTimeout(function () {
+    func();
+  }, delay);
+}
+
+//dev tool go for network tab and go for fetch tab and serch with all parameter u will see all query string item and passing and calling api...
+
+//--------------------------------------------------------------
 
 function append_movie(data) {
   let loder_div = document.getElementById("loader_div");

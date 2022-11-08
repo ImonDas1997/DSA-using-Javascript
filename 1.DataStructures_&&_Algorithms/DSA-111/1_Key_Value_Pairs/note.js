8955638107d022bd8931fdbe98b6122ddf136488
Babay <105317370+ImonDas1997@users.noreply.github.com>
2022-11-08 22:41:49 +0530

    .....

---
+++ 1.DataStructures_&&_Algorithms/DSA-111/1_Key_Value_Pairs/note.js
@@ -0,0 +1,19 @@
+// DSA 101 vs DSA 111
+//  More Focus on Problem Solving
+//  More Problem Statements
+//  Focus on Patterns in Problem Solving
+//  Various techniques and better approach
+
+// Key - Value Pairs
+//both are same  but in array we store only value and obj we store keynvalue pair...
+let arr = [0, 1, 2, 3]; //here index are use to get values
+let obj = { zero: 0, one: 1, two: 2, three: 3 }; //here key are use to get values
+//keys are flexible we can change them but idex we can not change
+
+console.log(arr[2]);
+
+console.log(obj["two"]);
+// we get same output
+
+//data type of index and keys-->
+//index are integer but keys  are strings

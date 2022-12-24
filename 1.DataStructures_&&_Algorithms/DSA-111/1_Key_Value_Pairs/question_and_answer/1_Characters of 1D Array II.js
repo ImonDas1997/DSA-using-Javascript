f89a941b6aa4f2c4574ff5011845fcb816446068
Saheli Ghosh <sahelig59@gmail.com>
2022-12-24 10:01:19 +0530

    ....

---
+++ 1.DataStructures_&&_Algorithms/DSA-111/1_Key_Value_Pairs/question_and_answer/1_Characters of 1D Array II.js
@@ -0,0 +1,50 @@
+// You are given a string, whose size is stored in a variable with the name N
+// The string is stored in a variable with the namestr
+
+// Each element of the string contains one of these symbols@,%,&,#,^
+
+// The symbols represent the following values
+
+// '@'-> 0
+// '%'-> 1
+// '&'-> 2 
+// '#'-> 3
+// '^'-> 4
+
+// You have to generate a string, such that each symbol is replaced by its corresponding values
+
+// For example, consider the value stored inN = 5, and the value stored instr = @%&#^
+
+// Therefore, replacing each value with its corresponding value, we get -01234, which is the required answer.
+ 
+//Input
+// The first line of the input contains the value stored inN
+
+// The next line stores the value stored instr
+
+
+// Output
+// Print the output as explained in the problem statement.
+
+// Sample Input 1 
+
+// 5
+// @%&#^
+// Sample Output 1
+
+// 01234
+// Hint
+
+// From the sample test case, the value stored inN = 5, and the value stored instr = @%&#^
+
+// Therefore, replacing each value with its corresponding value, we get -01234, which is the required answer.
+
+function char1DArray(N, str) {
+  let bag = "";
+  let sym = { "@": 0, "%": 1, "&": 2, "#": 3, "^": 4 };
+  for (let el of str) {
+    //console.log(sym[el])
+    bag += sym[el];
+  }
+  console.log(bag);
+}

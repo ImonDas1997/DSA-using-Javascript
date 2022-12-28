b94c44c486f476f414a29929f83887f93afcc59a
Saheli Ghosh <sahelig59@gmail.com>
2022-12-28 23:42:25 +0530

    ...

---
+++ 1.DataStructures_&&_Algorithms/DSA-111/1_Key_Value_Pairs/question_and_answer/8_problem2.js
@@ -0,0 +1,18 @@
+// Write a program to combine two objects by adding their values for common keys.
+
+// **Sample Input**
+// d1 = {a: 100, b: 200, c:300}
+// d2 = {a: 300, b: 200, d:400}
+
+// **Sample Output**
+// {a: 400, b: 400, c: 300, d: 400}
+
+// **Explanation**
+// d1 = {a: 100, b: 200, c:300}
+// d2 = {a: 300, b: 200, d:400}
+
+// In the above given objects, the value for key 'a' is 100 and 300
+// thus, we will calculate the sum 100 + 300 = 400 that will be the value for
+// key 'a' in final object
+// It will go same for other keys also
+// {a: 400, b: 400, c: 300, d: 400}

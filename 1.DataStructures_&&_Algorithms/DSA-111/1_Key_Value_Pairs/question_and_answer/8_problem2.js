c0d52161630946d6b0825bd3766304e8ef554021
Saheli Ghosh <sahelig59@gmail.com>
2022-12-29 17:00:44 +0530

    ...

--- 1.DataStructures_&&_Algorithms/DSA-111/1_Key_Value_Pairs/question_and_answer/8_problem2.js
+++ 1.DataStructures_&&_Algorithms/DSA-111/1_Key_Value_Pairs/question_and_answer/8_problem2.js
@@ -16,3 +16,14 @@
 // key 'a' in final object
 // It will go same for other keys also
 // {a: 400, b: 400, c: 300, d: 400}
+d1 = { a: 100, b: 200, c: 300 };
+d2 = { a: 300, b: 200, d: 400 };
+
+for (let key in d2) {
+  if (d1[key]) {
+    d1[key] += d2[key];
+  } else {
+    d1[key] = d2[key];
+  }
+}
+console.log(d1);
